3dc464a0fa1340025735745f4fc1b05d077f582b
JhonE17 <jhonepalacios17@gmail.com>
2024-01-30 22:57:02 -0500

    modifed config

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const baseDir = join(__dirname, '..');
 
-const cerPath = join(baseDir, '_fiel', process.env.CERPATH);
-const keyPath = join(baseDir, '_fiel', process.env.KEYPATH);
-const passwordPath = join(baseDir, '_fiel', process.env.PASSWORDPATH);
+// const cerPath = join(baseDir, '_fiel', process.env.CERPATH);
+// const keyPath = join(baseDir, '_fiel', process.env.KEYPATH);
+// const passwordPath = join(baseDir, '_fiel', process.env.PASSWORDPATH);
 
-export { cerPath, keyPath, passwordPath, baseDir};
+export { baseDir};
